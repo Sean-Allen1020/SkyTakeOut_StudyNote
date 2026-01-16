45b02301bffd42ec7348d2cffe41b51d597cca43
Hanatsu <zhuzhuzhu11330@gmail.com>
2026-01-17 01:00:55 +0900

    更新到生成jwt，以及利用全局异常处理器验证分别账号密码

--- 0.登录功能与文件上传/login-fileUpload-code/src/main/java/com/loginfileuploadcode/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.loginfileuploadcode.service;
-
-import com.loginfileuploadcode.pojo.Employee;
-import com.loginfileuploadcode.pojo.EmployeeLoginDTO;
-
-public interface EmployeeService {
-    Employee login(EmployeeLoginDTO employeeLoginDTO);
-}
