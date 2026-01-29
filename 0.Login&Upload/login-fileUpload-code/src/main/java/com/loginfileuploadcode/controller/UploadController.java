02ca863a5dfffdb17d7da80136300bfde1cd8f74
Hanatsu <zhuzhuzhu11330@gmail.com>
2026-01-29 16:42:38 +0900

    新增redis相关笔记，预设configuration笔记文件夹

--- 0.Login&Upload/login-fileUpload-code/src/main/java/com/loginfileuploadcode/controller/UploadController.java
+++ 0.Login&Upload/login-fileUpload-code/src/main/java/com/loginfileuploadcode/controller/UploadController.java
@@ -33,8 +33,8 @@ public class UploadController {
     public Result upload(MultipartFile file) throws IOException, ClientException {
         log.info("文件 {}", file);
         String url = aliyunOSSOperator.upload(file);
-        fileService.upload(url);
 
+        fileService.upload(url);
         return Result.success(url);
     }
 }
