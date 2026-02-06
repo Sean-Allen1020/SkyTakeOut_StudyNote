5dcff2f47aae31b3ab0b75aa8316c3aba4a58214
Hanatsu <zhuzhuzhu11330@gmail.com>
2026-02-06 17:16:41 +0900

    更新缓存相关笔记

---
+++ day07-Cache/springcache-demo/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.itheima.controller;
+
+import com.itheima.entity.User;
+import com.itheima.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @PostMapping
+    @CachePut(value = "userCache", key = "#user.id")    //如果用Cacheable的话，redis中在有对应键的情况下，就不会执行插入方法了
+//    @CachePut(value = "userCache", key = "#result.id")
+    public User save(@RequestBody User user){
+        userMapper.insert(user);
+        return user;
+    }
+
+    @DeleteMapping
+    @CacheEvict(value = "userCache", key = "#id")
+    public void deleteById(Long id){
+        userMapper.deleteById(id);
+    }
+
+	@DeleteMapping("/delAll")
+    @CacheEvict(value = "userCache", allEntries = true)
+    public void deleteAll(){
+        userMapper.deleteAll();
+    }
+
+    @GetMapping
+    @Cacheable(value = "userCache", key = "#id")
+    public User getById(Long id){
+        User user = userMapper.getById(id);
+        return user;
+    }
+
+}
